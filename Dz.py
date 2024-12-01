6c6e51553ec7dc0984d291db65d90dcc361d9b37
sirazh66 <sirazh007@gmail.com>
2024-12-01 19:00:29 +0300

    Add files via upload

---
+++ Dz.py
@@ -0,0 +1,5 @@
+first_name = "phyton"
+home_work = 12
+work_time_count = 1.5
+work_time = 0.125
+print(first_name,"всего задач", home_work,"затрачено часов", work_time_count,"среднее время выполнения", work_time)
